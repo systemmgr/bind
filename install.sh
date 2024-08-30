7d1024c9692fd3d559d24fd9cc0caad22405fa18
casjay <git-admin@casjaysdev.pro>
2024-08-29 22:41:29 -0400

    🗃️ Committing everything that changed 🗃️
    install.sh

--- install.sh
+++ install.sh
@@ -257,14 +257,10 @@ __run_post_install() {
     __chgroup 'named' /etc/named* /etc/rndc* /var/log/named /var/named /etc/logrotate.d/named 2>/dev/null
     echo "Installed on $(date)" >"/etc/named/.installed"
   fi
-  (
-    set -x
-    if __system_service_exists named; then
-      __system_service_enable named
-      __system_service_start named
-    fi
-    set +x
-  ) |& tee /tmp/debug_$APPNAME.log
+  if __system_service_exists named; then
+    __system_service_enable named
+    __system_service_start named
+  fi
   return $getRunStatus
 }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
